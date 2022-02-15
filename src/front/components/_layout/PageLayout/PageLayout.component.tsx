362f1fbab985f9e774da8154654e77701702c67f
DavidBabel <david.babel@iziwork.com>
2022-02-15 16:12:02 +0100

    fix footer position

--- src/front/components/_layout/PageLayout/PageLayout.component.tsx
+++ src/front/components/_layout/PageLayout/PageLayout.component.tsx
@@ -57,7 +57,7 @@ export function PageLayout({ children }: Props) {
         >
           <Header />
           <Content
-            style={{ margin: "0 16px", maxHeight: "85vh", overflow: "auto" }}
+            style={{ margin: "0 16px", maxHeight: "88vh", overflow: "auto" }}
           >
             <AutoBreadcrumb />
             <div
